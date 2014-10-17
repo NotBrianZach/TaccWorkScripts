aaf4bfca27a6b39b8228849e53ff5ff44320f2e1
ZachAbel <zachabel@zacsawesomesauce.gateway.pace.co>
2014-10-16 22:19:17 -0500

    got arrow working, need to get y coords and then embed

--- generateGraphs.sh
+++ generateGraphs.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #set -x
 #DIR=/work/01891/adb/cem/results/tacc_video
-DIR=/home/zachabel/tacc/ok_video
+DIR=/home/zachabel/TaccWorkScripts/ok_video
 mkdir ${DIR}/graphs
 
-#
-# gnuplot
-#
 function createGraph
 {
   graphFile=$1
@@ -15,9 +12,6 @@ function createGraph
   outFile=$4
   xAxis=$5
   yAxis=$6
-  echo $5
-  echo ""
-  echo ""
   GP_FILE=${graphFile}
   echo "#graph ${graphTitle}" > ${graphFile}
   #echo "set terminal postscript eps enhanced" >> ${GP_FILE}
@@ -30,7 +24,6 @@ function createGraph
   echo "set key top left" >> ${GP_FILE}
   #echo "unset key" >> ${GP_FILE} 
   # echo "set ytics 10"  >> ${GP_FILE}
-
 
   COLOR0="#0060ad"
   COLOR1="#888888"
@@ -57,14 +50,8 @@ function createGraph
   # echo "set yrange[0.007:100]" >> ${GP_FILE}
   echo "set grid lc rgb '#aaaaaa'" >> ${GP_FILE}
   echo "set pointsize 0.5" >> ${GP_FILE}
+  echo "set arrow from $7,50 to $7,0 linecolor rgb 'red'" >> ${GP_FILE}
   echo ""
-  #ZACH ADDING STUFF 
-  #echo "set style line 2 lt 1 lw 1" >> ${GP_FILE}
-  #echo "set style arrow 5 nohead ls 1" >> ${GP_FILE}
-  echo "set arrow from $7,50 to $7,0" >> ${GP_FILE}
-  #echo "set arrow lc rgb 'red'" >> ${GP_FILE}
-  echo "show arrow" >> ${GP_FILE}
-  #END ZACH ADDED STUFF
   echo -n "plot " >> ${GP_FILE}
   echo -n "'${datFile}' with lines linetype 1 linecolor rgb '${colors[${counter}]}'" >> ${graphFile}
   
@@ -99,12 +86,6 @@ function createGraph
   echo "set border linewidth 2" >> ${GP_FILE}
   echo "set style line 1 lc rgb '${COLOR0}' linetype 1 linewidth 5" >> ${GP_FILE}
 
-  #ZACH ADDING STUFF 
-  #create a red line
-  #echo "plot 9 rgb lc 'red'" >> ${GP_FILE}
-  #still need to set the dimensions with variables
-  #dst_[0-9][0-9] seems to be the thing to match
-  #END ZACH ADDED STUFF
   echo "set xlabel '$xAxis'" >> ${GP_FILE}
   echo "set ylabel '$yAxis'" >> ${GP_FILE}
   echo "set title '$graphTitle'" >> ${GP_FILE}
@@ -115,26 +96,31 @@ function createGraph
   echo ""
   echo -n "plot " >> ${GP_FILE}
   echo -n "'${datFile}' with lines linetype 1 linecolor rgb '${colors[${counter}]}'" >> ${graphFile}
+
+
 }
 
 #go through all the files in the directory
   for f in ${DIR}/*.txt
   do
     a=($(wc ${f}))
-    frames=${a[0]}#get number of lines in the file (which equals frames we'll need)
-    counter=0
-    #possibly add another loop right here to count max and min and pass in as parameter to function.
-    while [ $counter -le $frames ]
+    frames=${a[0]}
+    counterb=0
+    while [ $counterb -lt $frames ];
     do
-        echo $f  
-        datFile=${f}
-        shortName=`basename ${f} .txt`
-        echo $shortName
-        graphFile=${DIR}/graphs/$shortName.gnuplot
-        graphTitle=brisque_$shortName
-        createGraph $graphFile $graphTitle $datFile ${DIR}/graphs/$shortName.svg frame score $counter
-        echo "" >> ${graphFile}
-        gnuplot ${graphFile}
-        convert ${DIR}/graphs/${shortName}.svg  ${DIR}/graphs/${shortName}.png
+      echo $f  
+      datFile=${f}
+      shortName=`basename ${f} .txt`
+      echo $shortName
+      graphFile=${DIR}/graphs/$counterb$shortName.gnuplot
+      graphTitle=${counterb}brisque_$shortName
+      #graphFile=${DIR}/graphs/$shortName.gnuplot
+      #graphTitle=brisque_$shortName
+      createGraph $graphFile $graphTitle $datFile ${DIR}/graphs/$counterb$shortName.svg frame score $counterb
+      #createGraph $graphFile $graphTitle $datFile ${DIR}/graphs/$shortName.svg frame score
+      echo "" >> ${graphFile}
+      gnuplot ${graphFile}
+      convert ${DIR}/graphs/$counterb${shortName}.svg  ${DIR}/graphs/$counterb${shortName}.png
+      let counterb=counterb+1
     done
   done
